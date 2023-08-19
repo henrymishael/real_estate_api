74b4a641c574c4e57017abf28dee659a85a8e464
Mishael_eth <henrymishael.hm@gmail.com>
2023-08-19 23:18:38 +0100

    heroku deployment

--- django_project/settings.py
+++ django_project/settings.py
@@ -13,19 +13,23 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 from datetime import timedelta
+from environs import Env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+env = Env()
+env.read_env()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-hs(pnmunk!k-bt(apw$=t$p4j5qy@!%r)1yha$q6)tb84kfxdh"
+SECRET_KEY = env.str('SECRET_KEY')
+# "django-insecure-hs(pnmunk!k-bt(apw$=t$p4j5qy@!%r)1yha$q6)tb84kfxdh"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
@@ -85,15 +89,18 @@ WSGI_APPLICATION = "django_project.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "real_estate",
-        "USER": "postgres",
-        "PASSWORD": "Individualism777",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
-    }
+    "default": env.dj_db_url(
+        "DATABASE_URL"
+    )
 }
+# {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "real_estate",
+#         "USER": "postgres",
+#         "PASSWORD": "Individualism777",
+#         "HOST": "127.0.0.1",
+#         "PORT": "5432",
+#     }
 
 
 # Password validation
