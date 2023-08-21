498dd8f929247e5e6da76d0a679fa07d6d6c048d
Mishael_eth <henrymishael.hm@gmail.com>
2023-08-21 19:27:16 +0100

    tired

--- django_project/settings.py
+++ django_project/settings.py
@@ -90,8 +90,8 @@ WSGI_APPLICATION = "django_project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("DATABASE_URL"),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
