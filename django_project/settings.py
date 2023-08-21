c66540a808945c93418522b45c881492683b9477
Mishael_eth <henrymishael.hm@gmail.com>
2023-08-21 17:48:38 +0100

    secret key

--- django_project/settings.py
+++ django_project/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 import os
 from datetime import timedelta
 import environ
+from dotenv import load_dotenv
+
+load_dotenv()
 
 env = environ.Env(
     DEBUG=(bool, False),
@@ -26,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # "django-insecure-hs(pnmunk!k-bt(apw$=t$p4j5qy@!%r)1yha$q6)tb84kfxdh"
 
 # SECURITY WARNING: don't run with debug turned on in production!
